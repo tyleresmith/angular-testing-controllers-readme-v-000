9ecd62321d664d732349926d281c0a349eb6bb16
Tyler Smith <oats000@hotmail.com>
2016-06-16 23:04:02 +0000

    Done.

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -1,5 +1,9 @@
-describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
-    });
+describe('MainController', function() {
+  beforeEach(module('app'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
 });
